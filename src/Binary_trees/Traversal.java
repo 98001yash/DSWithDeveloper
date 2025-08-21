af947adafd399ecd9e4f4904a5152e6cda6d54cb
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-21 18:52:55 +0530

    solved the question size of the Node

---
+++ src/Binary_trees/Traversal.java
@@ -0,0 +1,17 @@
+package Binary_trees;
+
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val){
+        this.val  =val;
+    }
+}
+public class Traversal {
+    public static void main(String[] args){
+
+    }
+}
--- src/Binary_trees/Tree_01.java
+++ src/Binary_trees/Tree_01.java
@@ -1,17 +1,9 @@
 package Binary_trees;
 
 
-class Node{
-    int val;
-    Node left;
-    Node right;
-
-    Node(int val){
-        this.val  =val;
-    }
-}
 public class Tree_01 {
 
+    // to display the binary tree
     public static void display(Node root){
         //base case
         if(root ==null) return;
@@ -20,6 +12,7 @@ public class Tree_01 {
         display(root.right);
     }
 
+    // to find the sum of trees
     public static int nodeSum(Node root){
         if(root ==null) return 0;
        int x =  nodeSum(root.left);
@@ -28,6 +21,8 @@ public class Tree_01 {
        return root.val + x+ y;
     }
 
+
+    // to find the product of the tree
     public static int product(Node root) {
         if (root == null) return 1;
       int x =   product(root.left);
@@ -37,6 +32,8 @@ public class Tree_01 {
 
     }
 
+
+    // to find the product of the non-zero node
     public static int productNoZero(Node root){
         if(root ==null) return 1;
 
@@ -49,13 +46,31 @@ public class Tree_01 {
      return root.val* left * right;
     }
 
+
+    // to find the max value of the node of the tree
     public static int findMax(Node root){
         if(root ==null) return Integer.MIN_VALUE;
         int x = findMax(root.left);
         int y = findMax(root.right);
         int z = Math.max(x,y);
         return Math.max(root.val, z);
+    }
 
+
+    // to find the size of the tree
+    public static int size(Node root){
+        if(root ==null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+
+
+    // to find the level of the tree
+    public static int level(Node root){
+        if(root == null) return 0;
+        int x = level(root.left);
+        int y = level(root.right);
+        int z = Math.max(x,y);
+        return 1 +z;
     }
 
 
@@ -80,5 +95,7 @@ public class Tree_01 {
         System.out.println(product(a));
         System.out.println(productNoZero(a));
         System.out.println(findMax(a));
+
+        System.out.println(level(a));
     }
 }

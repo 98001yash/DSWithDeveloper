92c8b1bc27cc8ea9f4c09af84cc2c457f30741cf
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-31 12:41:04 +0530

    understood the conecpt of the constructor'

--- src/OOPS/TestApp_Inheritance.java
+++ src/OOPS/TestApp_Inheritance.java
@@ -1,14 +1,16 @@
 package OOPS;
 
-class Person {
+class Person {   // super class/ parent class
     public String name;
     public String address;
     public int age;
 }
 
 class Student1 extends Person{
+    // derived class. child class
 
     Student1(String name, String address, int age, int marks,String grade){
+        System.out.println("constructor called during object creation...! ");
         this.name = name;
         this.address = address;
         this.age = age;
@@ -25,7 +27,26 @@ class Student1 extends Person{
         System.out.println("Marks is: "+marks);
         System.out.println("Grade is: "+grade);
     }
+}
 
+class Student2 {
+
+    // instance variable
+    String name;
+    String address;
+    int age;
+    public int marks;
+    public String grade;
+
+
+    // constructor
+    Student2(String name, String address, int age, int marks, String grade){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.marks = marks;
+        this.grade = grade;
+    }
 }
 public class TestApp_Inheritance {
     public static void main(String[] args){
